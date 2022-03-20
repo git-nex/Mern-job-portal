0ffa1ee4bbbbc532f01328f16ebdf8d46ce77162
Aayush <vermaaayush68@gmail.com>
2022-03-21 01:08:41 +0530

    Changes

--- job_portal_mern-main/backend/server.js
+++ job_portal_mern-main/backend/server.js
@@ -4,10 +4,10 @@ const mongoose = require("mongoose");
 const passportConfig = require("./lib/passportConfig");
 const cors = require("cors");
 const fs = require("fs");
-
+const path = require("path")
 // MongoDB
 mongoose
-  .connect("mongodb://localhost:27017/jobPortal", {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -26,9 +26,9 @@ if (!fs.existsSync("./public/resume")) {
 if (!fs.existsSync("./public/profile")) {
   fs.mkdirSync("./public/profile");
 }
-
+require("dotenv").config()
 const app = express();
-const port = 4444;
+const port = process.env.PORT || 4444;
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
@@ -37,12 +37,18 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(cors());
 app.use(express.json());
 app.use(passportConfig.initialize());
-
+app.use(express.static(path.join(__dirname, "client", "build")))
 // Routing
 app.use("/auth", require("./routes/authRoutes"));
 app.use("/api", require("./routes/apiRoutes"));
 app.use("/upload", require("./routes/uploadRoutes"));
 app.use("/host", require("./routes/downloadRoutes"));
+
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}!`);
